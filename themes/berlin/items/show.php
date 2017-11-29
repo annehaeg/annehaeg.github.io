6ceb7bf4a82a59f42d2b11e934caeed0a0f2a967
Annelien Haeghebaert <annelien@Anneliens-MacBook-Pro.local>
2017-11-29 13:16:08 +0100

    itemviewer + pfd implementatie

--- themes/berlin/items/show.php
+++ themes/berlin/items/show.php
@@ -35,11 +35,20 @@
        <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
 
 
-    <ul class="item-pagination navigation">
-        <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
-        <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
-    </ul>
+<!--    <ul class="item-pagination navigation">-->
+<!--        <li id="previous-item" class="previous">--><?php //echo link_to_previous_item_show(); ?><!--</li>-->
+<!--        <li id="next-item" class="next">--><?php //echo link_to_next_item_show(); ?><!--</li>-->
+<!--    </ul>-->
+
+    <nav>
+        <ul class="item-pagination navigation">
+            <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
+            <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
+        </ul>
+    </nav>
+
 
 </div> <!-- End of Primary. -->
+
 
  <?php echo foot(); ?>
